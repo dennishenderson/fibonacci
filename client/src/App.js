03507743a2b4c82842650270aa1af6bb0f68b0e7
Dennis M Henderson <dennismhenderson@gmail.com>
2022-11-25 19:23:35 -0500

    modified app.js to see changed to main page to re-run full ci/cd process

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React Today
+            React Presented To You By Dennis Henderson
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
